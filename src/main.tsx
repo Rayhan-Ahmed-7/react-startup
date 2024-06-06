baad17bda621a117db8d7b73f6ab82abc5ee7b29
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2024-06-06 23:34:22 +0600

    :zap: done nested routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,67 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Link,
+  Outlet,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <nav>
+        <Link to="stats">Stats</Link>
+        <Link to="reports">Reports</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
 
+export default Dashboard;
+
+function Stats() {
+  return <h1>Stats Page</h1>;
+}
+
+function Reports() {
+  return <h1>Reports Page</h1>;
+}
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{}],
+    children: [
+      {
+        index: true,
+        element: <div>DashBoard</div>,
+      },
+      {
+        path: "nested",
+        element: <Dashboard />,
+        children: [
+          {
+            path: "stats",
+            element: <Stats />,
+          },
+          {
+            path: "reports",
+            element: <Reports />,
+          },
+        ],
+      },
+      {
+        path: "invoice",
+        element: <div>invoice</div>,
+        children: [],
+      },
+      {
+        path: "customer",
+        element: <div>customer</div>,
+      },
+    ],
   },
 ]);
 
